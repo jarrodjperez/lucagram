cd6199b13a3a42bf4fe72526dd09d39c015d27fb
Jarrod Perez <perez.jarrod@gmail.com>
2021-07-25 19:11:20 -0500

    update env variables

--- s3.ts
+++ s3.ts
@@ -2,8 +2,8 @@ import aws from "aws-sdk";
 
 const region = "us-east-1";
 const bucketName = "lucagram";
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+const accessKeyId = process.env.S3_UPLOAD_KEY;
+const secretAccessKey = process.env.S3_UPLOAD_SECRET;
 
 const s3 = new aws.S3({
   region,
