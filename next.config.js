79b7c43fcddba67afb5c9e663f5500ba3275af5a
Jarrod Perez <perez.jarrod@gmail.com>
2022-03-27 23:09:24 -0500

    update deps

--- next.config.js
+++ next.config.js
@@ -2,14 +2,5 @@ module.exports = {
   swcMinify: true,
   images: {
     domains: [`${process.env.S3_UPLOAD_BUCKET}.s3.amazonaws.com`],
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/feed",
-        permanent: true,
-      },
-    ];
-  },
+  }
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,24 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import useSWR from "swr";
+import Footer from "../components/Footer";
 import FullPageLoader from "../components/FullPageLoader";
+import Header from "../components/Header";
+import PostCard from "../components/PostCard";
+import { fetcher } from "../lib/fetcher";
 
 export default function Index() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push("/feed");
-  }, []);
+  const { data: posts } = useSWR("/api/posts", fetcher);
 
-  return <FullPageLoader />;
+  if (!posts) return <FullPageLoader />;
+
+  return (
+    <div className="sm:container mx-auto relative">
+      <Header />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:gap-10 overflow-hidden items-center pb-12 sm:pb-4 pt-0 sm:pt-2">
+        {posts && posts.map((post) => <PostCard key={post.id} post={post} />)}
+      </div>
+      <Footer />
+    </div>
+  );
 }
